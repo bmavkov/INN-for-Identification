56c5cc7f4b56d5fde8820d9f32fa7c1edaa90a8d
marco <marco.forgione@supsi.ch>
2020-04-27 22:49:23 +0200

    code improvements

--- INN code/NNmodels.py
+++ INN code/NNmodels.py
@@ -25,34 +25,14 @@ class NeuralStateSpaceModel(nn.Module):
         self.n_feat = n_feat
         self.net = nn.Sequential(
             nn.Linear(n_x+n_u, n_feat[0]),  # 2 states, 1 input
-            #nn.Dropout(0.2),
-            #nn.ReLU6(),
             nn.Sigmoid(),
-           # nn.SELU(),
             nn.Linear(n_feat[0], n_feat[1]),
-         #  #  nn.Dropout(0.2),
-           # nn.ReLU6(),
             nn.Sigmoid(),
-           # nn.ReLU(),
-         #    #nn.SELU(),
-           # nn.Linear(n_feat[1], n_feat[2]),
-         # #   nn.Dropout(0.2),
-          #  nn.Sigmoid(),
-           # nn.ReLU(),
-         #    nn.Linear(n_feat, n_feat),
-         #    nn.Sigmoid(),
-         #    nn.Linear(n_feat, n_feat),
-         #    nn.Sigmoid(),
-         #    nn.Linear(n_feat, n_feat),
-         #    nn.Sigmoid(),
-            nn.Linear(n_feat[2], n_x),
-         #   nn.ReLU()
+            nn.Linear(n_feat[1], n_x),
         )
 
-       
-    
-    def forward(self, X,U):
-        XU = torch.cat((X,U),-1)
+    def forward(self, X, U):
+        XU = torch.cat((X, U), -1)
         DX = self.net(XU)
         return DX
     
@@ -62,17 +42,15 @@ class INN:
     def __init__(self, nn_model):
         self.nn_model = nn_model
 
-    def INN_est(self, X_est,U,dt):
-        X_est_torch = self.nn_model(X_est.float(),U.float())
-        X_sum = torch.cumsum(X_est_torch, dim=0)
-        x0=X_est[0,:]
-        xdot_int=torch.add(x0,dt*X_sum)
- 
+    def INN_est(self, X_hat, U, dt):
+        X_dot = self.nn_model(X_hat, U)
+        DX = dt*torch.cumsum(X_dot, dim=0)
+        x0 = X_hat[0, :]
+        X_hat_I = x0 + DX
 
-        return xdot_int
+        return X_hat_I
 
-    
-    
+
 class NeuralStateSpaceModel_y(nn.Module):
    
     def __init__(self, n_x, n_y, n_feat=64):
@@ -82,28 +60,10 @@ class NeuralStateSpaceModel_y(nn.Module):
         self.n_feat = n_feat
         self.net = nn.Sequential(
             nn.Linear(n_x, n_feat),  # 2 states, 1 input
-           # nn.Dropout(0.2),
             nn.ReLU(),
-          #  nn.Sigmoid(),
-           # nn.SELU(),
-         #   nn.Linear(n_feat, n_feat),
-          #  nn.Dropout(0.2),
-            #nn.ReLU(),
-         #   nn.Sigmoid(),
-            #nn.SELU(),
-        #    nn.Linear(n_feat, n_feat),
-         #   nn.Dropout(0.2),
-          #  nn.ReLU(),
-       #     nn.Sigmoid(),
-         #   nn.Linear(n_feat, n_feat),
-          #  nn.Sigmoid(),
             nn.Linear(n_feat, n_y)
         )
 
-       
-    
     def forward(self, X):
-        DX = self.net(X)
-        return DX
-    
-  
+        Y = self.net(X)
+        return Y
